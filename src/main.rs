f0cb7fed8bc22cbd223f1016afce749e84d17bad
james <jamesbeech2003@hotmail.com>
2025-06-11 14:54:12 +0400

    Restructured main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,23 @@ mod map;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-
-    //Structure
-
     //Get current gps position
+    let lat = 25.2048;
+    let lon = 55.2708;
+    let zoom = 15;
+
     //Convert gps to map tile and pixel coordinates
+    let (x_tile, y_tile) = map::gps_to_tile(lat, lon, zoom);
+    println!("Tile X: {}, Tile Y: {}, Zoom: {}", x_tile, y_tile, zoom);
+
+    match map::fetch_tile(x_tile, y_tile, zoom).await {
+        Ok(_) => println!("✅ Tile successfully fetched or loaded."),
+        Err(e) => eprintln!("❌ Error fetching tile: {}", e),
+    }
+
     //Load surrounding map tiles
     //Render Map Centered on user
     //Draw Overlays
     //Present Frame
 
-let lat = 25.2048;
-let lon = 55.2708;
-let zoom = 15;
-
-let (x_tile, y_tile) = map::gps_to_tile(lat, lon, zoom);
-println!("Tile X: {}, Tile Y: {}, Zoom: {}", x_tile, y_tile, zoom);
-
-match map::fetch_tile(x_tile, y_tile, zoom).await {
-    Ok(_) => println!("✅ Tile successfully fetched or loaded."),
-    Err(e) => eprintln!("❌ Error fetching tile: {}", e),
-}
 }
