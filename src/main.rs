681f8c3c5cf5d8a7330ddca3226cd60c5e72b5a3
james <jamesbeech2003@hotmail.com>
2025-06-14 15:43:43 +0400

    Moved combine_images to the renderer class to keep code modular, added method comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use image::{RgbaImage};
-use show_image::{create_window, ImageInfo, ImageView};
-
 mod map;
 mod renderer;
 
@@ -20,34 +17,14 @@ async fn main() {
         let _ = map::fetch_tile(x, y, zoom).await;
     }
 
-    // Compose all tiles into one image
+    // Convert tiles from Vec<(u32, u32)> to Vec<(i32, i32)>
+    let tiles_i32: Vec<(i32, i32)> = tiles.iter().map(|&(x, y)| (x as i32, y as i32)).collect();
+
+    renderer::combine_images(&tiles_i32, tile_size, zoom.into());
+
     
-    let grid = (tiles.iter().map(|(_x, _y)| _x).max().unwrap() - tiles.iter().map(|(_x, _y)| _x).min().unwrap() + 1) as u32;
-    let mut canvas = RgbaImage::new(grid * tile_size, grid * tile_size);
 
-    let min_x = tiles.iter().map(|(x, _)| *x).min().unwrap();
-    let min_y = tiles.iter().map(|(_, y)| *y).min().unwrap();
-
-    for &(x, y) in &tiles {
-        let tile_path = map::tile_path(x, y, zoom);
-        if let Ok(tile_img) = renderer::load_tile(tile_path.as_path()) {
-            let dx = ((x - min_x) as u32) * tile_size;
-            let dy = ((y - min_y) as u32) * tile_size;
-            image::imageops::overlay(&mut canvas, &tile_img, dx.into(), dy.into());
-        }
-    }
-
-    // Show the composed image in a single window
-    show_image::run_context(move || -> Result<(), Box<dyn std::error::Error>> {
-        let window = create_window("Map", Default::default()).unwrap();
-        window.set_image(
-            "map",
-            ImageView::new(ImageInfo::rgba8(canvas.width(), canvas.height()), &canvas),
-        ).unwrap();
-        window.wait_until_destroyed().unwrap();
-        Ok(())
-    });
-
+    
     // Wait until the window is closed
     
 }
